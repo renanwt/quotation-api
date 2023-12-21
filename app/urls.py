b2921cc952fbfe1f1876ab983158b279ad5f643c
renanwt <renanwt@gmail.com>
2023-12-20 23:52:11 -0300

    Highcharts added

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import FinancialRecordList
+from .views import FinancialRecordGetPostView, HighchartsView
 
 urlpatterns = [
-    path('records/', FinancialRecordList.as_view()),
+    path('records/', FinancialRecordGetPostView.as_view(), name='get-post-records'),
+    path('chart/', HighchartsView.as_view(), name='chart-view'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,3 +1,5 @@
+from django.shortcuts import render
+import requests
 from rest_framework import generics, status
 from rest_framework.response import Response
 from .models import FinancialRecord
@@ -7,7 +9,7 @@ from .utils import dollar_to_all, date_validator, currency_validator
 from datetime import date, timedelta
 
 
-class FinancialRecordList(generics.GenericAPIView):
+class FinancialRecordGetPostView(generics.GenericAPIView):
     serializer_class = FinancialRecordGetAllSerializer
     queryset = FinancialRecord.objects.all()
 
@@ -73,13 +75,13 @@ class FinancialRecordList(generics.GenericAPIView):
                 start_date = end_date - timedelta(days=4)
         try:
             if not currency_param:
-                queryset = FinancialRecord.objects.filter(date__range=(str(start_date), str(end_date)))
+                queryset = FinancialRecord.objects.filter(date__range=(str(start_date), str(end_date))).order_by('date')
                 serializer_class = self.serializer_class
             elif currency_param.lower() == 'usd':
                 return Response("USD quotation in USD is 1.00. Please try 'BRL', 'EUR' or 'JPY'.",
                                 status=status.HTTP_200_OK)
             else:
-                queryset = FinancialRecord.objects.filter(date__range=(str(start_date), str(end_date)))
+                queryset = FinancialRecord.objects.filter(date__range=(str(start_date), str(end_date))).order_by('date')
                 serializer_class = {
                     'brl': FinancialRecordGetBRLSerializer,
                     'eur': FinancialRecordGetEURSerializer,
@@ -94,3 +96,41 @@ class FinancialRecordList(generics.GenericAPIView):
 
         except Exception as e:
             return Response(f"Failed getting quotation: {e}", status=status.HTTP_400_BAD_REQUEST)
+
+
+class HighchartsView(generics.GenericAPIView):
+
+    def get(self, request):
+        # Extract query parameters
+        start_date_param = request.query_params.get('start_date', '')
+        end_date_param = request.query_params.get('end_date', date.today())
+        currency_param = request.query_params.get('currency', None)
+
+        start_date = end_date = None
+
+        if currency_param and not currency_validator(currency_param):
+            return Response("Not valid currency. It must be: 'brl', 'eur' or 'jpy.', status=400")
+
+        if start_date_param and end_date_param:
+            if not date_validator(start_date_param, end_date_param):
+                return Response("Difference between end_date and start_date should be at most 5 days.", status=400)
+            start_date, end_date = start_date_param, end_date_param
+        else:
+            if start_date_param:
+                start_date = date.fromisoformat(str(start_date_param))
+                end_date = start_date + timedelta(days=4)
+            else:
+                end_date = date.fromisoformat(str(end_date_param))
+                start_date = end_date - timedelta(days=4)
+        try:
+            response = requests.get('http://127.0.0.1:8000/api/records', params={'start_date': start_date,
+                                                                                 'end_date': end_date,
+                                                                                 'currency': currency_param})
+        except Exception as e:
+            return Response(f"Failed getting quotation: {e}", status=status.HTTP_400_BAD_REQUEST)
+        if response.status_code == 200:
+            financial_data = response.json()
+            return render(request, 'index.html', {'chart_data': financial_data})
+        else:
+            error_message = f"Failed to get financial data. Status code: {response.status_code}"
+            return render(request, 'error.html', {'error_message': error_message})
